ed6217ad9e4bd13a9fc638ab0426d3497e6a494c
JH <jhk3261@gnu.ac.kr>
2025-09-25 11:52:24 +0900

    ✨ feat[#14] : 검색 DTO, 함수 생성

---
+++ src/api/search/searchApi.ts
@@ -0,0 +1,64 @@
+// src/api/search/searchApi.ts
+import instance from '../axiosInstance';
+import type { BaseResponse } from '../baseResponse';
+
+// 검색 전
+export type SearchAllDTO = {
+  recommendKeyword: string[];
+  livePopularSearch: string[];
+  nowTime: string; // "2025.09.16 16:16:42"
+};
+
+export type SearchComposerDTO = {
+  name: string[];
+  imgLink: string[];
+};
+
+export type SearchPlayerDTO = {
+  name: string;
+  keyword: string[];
+};
+
+export type SearchGenreDTO = {
+  genre: string;
+  keyword: string[];
+};
+
+export type SearchPeriodDTO = {
+  era: string[];
+};
+
+export type SearchInstrumentDTO = {
+  instrument: string[];
+  imgLink: string[];
+};
+
+export const fetchSearchAll = async (): Promise<SearchAllDTO> => {
+  const res = await instance.get<BaseResponse<SearchAllDTO>>('/api/search/all');
+  return res.data.data;
+};
+
+export const fetchSearchComposer = async ():Promise<SearchComposerDTO> => {
+  const res = await instance.get<BaseResponse<SearchComposerDTO>>('/api/search/composer');
+  return res.data.data;
+}
+
+export const fetchSearchPlayer = async ():Promise<SearchPlayerDTO[]> => {
+  const res = await instance.get<BaseResponse<SearchPlayerDTO[]>>('/api/search/player');
+  return res.data.data;
+}
+
+export const fetchSearchGenre = async ():Promise<SearchGenreDTO[]> => {
+  const res = await instance.get<BaseResponse<SearchGenreDTO[]>>('/api/search/genre');
+  return res.data.data;
+}
+
+export const fetchSearchPeriod = async ():Promise<SearchPeriodDTO> => {
+  const res = await instance.get<BaseResponse<SearchPeriodDTO>>('/api/search/period');
+  return res.data.data;
+}
+
+export const fetchSearchInstrument= async ():Promise<SearchInstrumentDTO> => {
+  const res = await instance.get<BaseResponse<SearchInstrumentDTO>>('/api/search/instrument');
+  return res.data.data;
+}
